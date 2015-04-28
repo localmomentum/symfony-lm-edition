2820170c99b174c1c8f363165e62df9d53cf7772
Johnny Robeson <johnny@localmomentum.net>
2015-04-28 04:06:29 -0400

    update index.php to match current usage

--- web/index.php
+++ web/index.php
@@ -1,29 +1,35 @@
 <?php
-
-use Symfony\Component\ClassLoader\ApcClassLoader;
+/**
+ * Symfony App Front Controller
+ *
+ * @todo consider using the ApcClassLoader
+ */
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
+require_once __DIR__.'/../app/autoload.php';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$apcLoader = new ApcClassLoader('sf2', $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+if (file_exists(__DIR__.'/../.env')) {
+    $dotEnv = new \Dotenv\Dotenv();
+    $dotEnv->load(__DIR__.'/../');
+}
+$env = $_SERVER['SYMFONY_ENV'];
+$debug = (bool)$_SERVER['SYMFONY_DEBUG'];
 
+if ('prod' == $env) {
+    require_once __DIR__.'/../var/bootstrap.php.cache';
+}
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppKernel($env, $debug);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
+
+if (function_exists('register_postsend_function')) {
+    register_postsend_function(function () use ($kernel, $request, $response) {
 $kernel->terminate($request, $response);
+    });
+} else {
+    $kernel->terminate($request, $response);
+}
